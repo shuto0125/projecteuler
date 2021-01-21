bcf4b1ea0008a1616c7fd3bb189f60b0f54f2f90
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2021-01-21 16:55:54 +0900

    problem1_refine

--- problem1/index.rb
+++ problem1/index.rb
@@ -1,15 +1,25 @@
 
-
 total = 0
 
 #  1~1000 をループする
-
 for num in 1..1000 do
 
     #3 か 5 の倍数になっている数字の場合はtotal に加算する
     if num % 3 == 0 || num % 5 == 0
         total += num
     end
+
 end
 
+p total
+
+
+total = 0
+1000.times do |i|
+    num = i + 1
+    #3 か 5 の倍数になっている数字の場合はtotal に加算する
+    if num % 3 == 0 || num % 5 == 0
+        total += num
+    end
+end
 p total
