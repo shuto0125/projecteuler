f9cce88362c538830e6d4620c0cdc2091559aceb
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2021-01-23 13:40:49 +0900

    problem3-refine

--- problem3/index.rb
+++ problem3/index.rb
@@ -1,50 +1,58 @@
 
 target = 600851475143
+# target = 600851
 
-num_prime = Array.new(1)
+# num_prime = [1,2]
+
+my_prime = []
 
 # 素数を出す クラス https://docs.ruby-lang.org/ja/latest/class/Prime.html
-# require 'prime'
+require 'prime'
 
 # Prime.each(100) do |prime|
 #   p prime
 # end
 
-# yakusuu=0;
-# for(j=1;j<=i;j++)
-# {
-# 	if(i%j==0) yakusuu++;
-# }
+5000000.times do |i|
+
+  i += 1
+
+  if target != 0
+
+    if target % i == 0
+      my_prime.push(i)
+      target = target / i
+      p target
+
+    end
+
+  end
+  
+
+end
+
+p my_prime
+
+target = my_prime.last
+
 
 
 
 
 target.times do |i|
+  i += 1
+  # p i
+  if target != 0
 
-  yakusuu=0;
+    if target % i == 0
+      my_prime.push(i)
+      target = target / i
+      p target
 
-  #奇数の場合のみ処理
-  if i.odd?
-
-    for j in 1..i do
-    
-      if i%j==0
-        yakusuu += 1;
-      end
-
-    end
-
-    
-    #約数が2だけの場合は出力（素数の場合）
-    if yakusuu==2
-      p i #処理が動いてるか確認の為
-      num_prime.push(i);
     end
 
   end
-
-  i += 1
-
 end
 
-p num_prime.last
+p Prime.prime?(my_prime.last) 
+p my_prime.last
